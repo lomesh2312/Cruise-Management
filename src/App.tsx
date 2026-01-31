a34529a30eb6204b9b5893facf3643669d1600b4
lomesh2312 <lomesh.sonkeshriya@adypu.edu.in>
2026-01-31 10:49:50 +0530

    video replace

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import cruise from './assets/cruise.mp4'
+import cruise1 from './assets/cruise1.mp4'
 
 function App() {
  
@@ -20,7 +20,7 @@ function App() {
     bottom: 0,
     zIndex: -1,
   }} >
-          <source src={cruise} type="video/mp4" />
+          <source src={cruise1} type="video/mp4" />
         </video>
         </div>
       </div>
